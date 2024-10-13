1f658eae374036acf64dbaba63783e245a9b995b
GiHwan2 <sandwich5011@naver.com>
2024-10-13 19:57:12 +0900

    chore: 초기 개발 환경 세팅

---
+++ src/test/java/com/eduai/eduping/EdupingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduai.eduping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdupingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
